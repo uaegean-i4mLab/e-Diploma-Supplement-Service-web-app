44bc640eca01fa848cdfc592c787f415daf7042c
Nikos <triantafyllou.ni@gmail.com>
2018-06-04 18:58:30 +0300

    fallback to config file if no env properties are defined

--- src/main/java/gr/uagean/dsIss/service/impl/ParameterServiceImpl.java
+++ src/main/java/gr/uagean/dsIss/service/impl/ParameterServiceImpl.java
@@ -6,7 +6,16 @@
 package gr.uagean.dsIss.service.impl;
 
 import gr.uagean.dsIss.service.ParameterService;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 /**
  *
@@ -15,9 +24,44 @@ import org.springframework.stereotype.Service;
 @Service
 public class ParameterServiceImpl implements ParameterService {
 
+    private final Logger log = LoggerFactory.getLogger(ParameterServiceImpl.class);
+
+    private final Map<String, String> properties;
+
+    public ParameterServiceImpl() {
+        properties = getConfigProperties();
+    }
+
     @Override
     public String getParam(String paramName) {
+        if (StringUtils.isEmpty(System.getenv(paramName))) {
+            return properties.get(paramName);
+        }
         return System.getenv(paramName);
+    }
+
+    private Map<String, String> getConfigProperties() {
+        Properties prop = new Properties();
+        InputStream input = null;
+        HashMap<String, String> map = new HashMap();
+        try {
+            input = new FileInputStream("/webappConfig/config.properties");
+            prop.load(input);
+            prop.forEach((key, value) -> {
+                map.put((String) key, (String) value);
+            });
+        } catch (IOException ex) {
+            log.info("Properties file not found in /webappConfig/config.properties", ex);
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return map;
     }
 
 }
